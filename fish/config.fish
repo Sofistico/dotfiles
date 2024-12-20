80d5a6d25098b5721680879050d96b7ade54055a
Sofistico <59299346+Sofistico@users.noreply.github.com>
2024-12-20 15:13:46 -0300

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,11 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-zoxide init fish | source
+fish_add_path -m /home/sofistico/.local/bin
+fish_add_path -m ~/.local/bin
+
 alias v="nvim"
 
 starship init fish | source
+zoxide init fish | source
+fzf --fish | source
