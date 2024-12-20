d6e34b3734915ffc1ec9a3fbcccf110a865ed718
Sofistico <59299346+Sofistico@users.noreply.github.com>
2024-12-20 15:26:48 -0300

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fish_add_path -m /home/sofistico/.local/bin
 fish_add_path -m ~/.local/bin
 
